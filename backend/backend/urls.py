5ddf120d29ed82c22aaec24ea6bafebbe7c6c0de
PierreSaumet <psaumet.dvp@gmail.com>
2026-01-14 12:01:18 +0100

    add ing healtchek

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from backend.views import health_check
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health_check'),
     path('api/', include("blogs.urls")),
     path('api/', include("users.urls")),
 ]
